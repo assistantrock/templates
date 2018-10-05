cef86c1858b22dcd1b7ac00cb803830ccc75cf29
Roger Machado <43892109+rogermachado3d@users.noreply.github.com>
2018-10-05 16:34:56 -0300

    Add files via upload

---
+++ renameToYourTask.js
@@ -0,0 +1,31 @@
+/* 
+@@@@@ Copyright AssistantRocks LTDA Brazil / Pelotas-RS 2018
+@@@@File devloped by (PUT YOUR NAME HERE) as a freelancer work or a "in house" devlopment 
+*/
+
+//@@@ declare the requeriments like the taks especifications.
+
+var _ = require("lodash")
+
+
+//@@@ declare the variables for that taks
+var x  = "";
+
+
+
+//@@@ here declare the 2 principals functions, one for input and other for output
+//@@@ log all results or comment then too. Use the examples.
+
+/* OUTPUT FUNCTION */
+function output() {
+    
+}
+
+
+/* INPUT FUNCTION */
+
+function input(params) {
+    console.log("here start the params --->", params)
+}
+
+//@@@ here declare all the others rules for complete the task
---
+++ renameToYourTask.php
@@ -0,0 +1,40 @@
+<?php
+/*
+Plugin Name:  AssistantRocks Woo
+Plugin URI:   https://assistantrocks.com/
+Description:  Project Amadeuz
+Version:      001000
+Author:       AssistantRocks
+Author URI:   https://assistantrocks.com/
+License:      temp
+License URI:  https://www.temp.org/temp.html
+Text Domain:  temp
+Domain Path:  /languages
+
+{AssistantRocks Woo} is a software: you can use this software to interact with others system of 
+assistantrocks.
+
+@@@@@ Copyright AssistantRocks LTDA Brazil / Pelotas-RS 2018
+@@@@ File devloped by (PUT YOUR NAME HERE) as a freelancer work or a "in house" devlopment
+
+*/
+
+//@@@ declare the variables for that taks, if need
+
+
+
+//@@@ here declare the 2 principals functions, one for input and other for output
+//@@@ comment then too. Use the examples.
+
+/* OUTPUT FUNCTION */
+
+
+/* INPUT FUNCTION */
+
+
+
+
+//@@@ here declare all the others rules for complete the task
+//@@@ comment then too. Use the examples.
+
+ ?>
